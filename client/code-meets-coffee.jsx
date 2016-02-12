a87ff0bce2ecbc4766a20ef60c44a776e15c1a87
scalkpdev <garvin.ling@kp.org>
2016-02-12 01:15:26 -0800

    Removing manual auth function for github
    Removing manual auth function

--- client/code-meets-coffee.jsx
+++ client/code-meets-coffee.jsx
@@ -5,41 +5,6 @@ SavedRepos = new Mongo.Collection("fastrepos");
 if(Meteor.isClient) {	
 
 
-		var checkAuth = function() {
-			
-			var sessionCode = Session.get('github_code');
-
-
-			if(sessionCode === undefined) { 
-
-				var code = window.location.href.match(/\?code=(.*)/);
-			
-				if(code !== null){
-					
-					console.log('Got user code.  Setting new session');
-					Session.set('github_code',code[1]);
-				   
-				    Meteor.call("authorizeCode",code[1],function(err,data){
-
-				   			 Meteor.call("starRepo","airbnb","javascript");
-				   			 Meteor.call("testAuth")
-
-				    });
-
-				} else {
-					console.log('user not logged in. liking disabled.')
-
-				}
-
-			}  else {
-
-				console.log('session has been set. ')
-
-			}
-
-		};
-
-
 		Meteor.startup(function(){
 
 		
